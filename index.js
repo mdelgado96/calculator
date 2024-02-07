b8b098dcb68e11fceb042c2ca1286fcbcd1eeb74
Marvin Delgado <d.marvin96@gmail.com>
2024-02-07 12:37:52 -0800

    Setting up DOM M.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+function entered(number) {
+  document.getElementById("screen").number += number;
+}
