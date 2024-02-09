cebdb9e4ef1fab09b456429ffca5f76f7e6f314e
Marvin Delgado <d.marvin96@gmail.com>
2024-02-08 19:54:29 -0800

    numbers now appear on the dom accordingly

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function entered(number) {
-  document.getElementById("screen").number += number;
+  let count = 0;
+  if (number == ".") {
+    count++;
+  }
+
+  if (count > 1) {
+  }
+  document.getElementById("screen").firstChild.nodeValue = number;
 }
