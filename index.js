b510c2bacb02c8b559a3582d5afa96639a4f653d
Marvin Delgado <d.marvin96@gmail.com>
2024-03-07 18:56:57 -0800

    documentation

--- index.js
+++ index.js
@@ -3,36 +3,44 @@ let togglelight = false;
 let lastNum = "";
 
 function entered(number) {
+  // count for the number of decimal points
   let count = 0;
 
+  // adds numbers to the screen
   const current = (document.getElementById("screen").firstChild.nodeValue +=
     number);
 
+  // removes initial 0
   if (current.length < 3 && current[0] == "0") {
     document.getElementById("screen").firstChild.nodeValue -= "";
   }
 
+  // counts number of decimal points
   for (let i = 0; i < current.length; i++) {
     if (current[i] == ".") {
       count++;
     }
   }
 
+  // if extra decmila is added then resets number on screen
   if (count > 1) {
     document.getElementById("screen").firstChild.nodeValue = "0";
   }
 }
 
+// clears everything on the screen and previous numger
 function clearAll() {
   document.getElementById("screen").firstChild.nodeValue = "0";
   document.getElementById("last_num").innerText = "";
 }
 
+// only clears number currently on the screen
 function clear() {
   console.log("click");
   document.getElementById("screen").firstChild.nodeValue = "0";
 }
 
+// adds operators to previous number to get ready for operation
 function operator(op) {
   document.getElementById("screen").firstChild.nodeValue += op;
 
@@ -42,8 +50,9 @@ function operator(op) {
   document.getElementById("screen").firstChild.nodeValue = "0";
 }
 
+// performs operation
 function equals() {
-  // lastNum += document.getElementById("screen").firstChild.nodeValue;
+  lastNum += document.getElementById("screen").firstChild.nodeValue;
 
   try {
     document.getElementById("screen").firstChild.nodeValue = eval(lastNum);
@@ -53,6 +62,7 @@ function equals() {
   }
 }
 
+// toggles to dark theme
 function toggleDark() {
   togglelight = !togglelight;
   if (togglelight) {
@@ -63,6 +73,7 @@ function toggleDark() {
   toggledark = true;
 }
 
+// toggles to light theme
 function toggleLight() {
   if (toggledark) {
     toggledark = false;
